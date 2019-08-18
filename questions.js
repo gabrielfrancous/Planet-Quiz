323905ef3907c70d2a518726b1c5533d963fa8c8
gabrielfrancous <52275539+gabrielfrancous@users.noreply.github.com>
2019-08-18 12:07:18 -0700

    Create questions.js
    List of questions for quiz.

---
+++ questions.js
@@ -0,0 +1,52 @@
+let questions = [
+  {
+      question: 'What planet spins in reverse order in relation to all the others?',
+      answers: [
+        'Mecury',
+        'Venus',
+        'Earth',
+        'Mars'
+        ],
+      right: 'Venus'
+    },
+     {
+      question: 'What planet has its spinning axis almost parallel to its orbit around the Sun?',
+      answers: [
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+        ],
+      right: 'Uranus'
+    },
+     {
+      question: 'What is the least dense planet in the solar system?',
+      answers: [
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+        ],
+      right: 'Saturn'
+    },
+     {
+      question: 'What is the location of the asteroid belt?',
+      answers: [
+        'Between Earth and Mars',
+        'Between Mars and Jupiter',
+        'Between Jupiter and Saturn',
+        'Between Saturn and Uranus'
+        ],
+      right: 'Between Mars and Jupiter'
+    },
+     {
+      question: 'What was the first planet to form?',
+      answers: [
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+        ],
+      right: 'Jupiter'
+    },
+];
